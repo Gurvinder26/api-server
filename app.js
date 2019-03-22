700895c8733c4ab120706da40b41efc672cf4b33
Gurvinder singh <gurayasaab@hotmail.com>
2019-03-22 13:00:18 -0700

    feat(app): error handling for the routes

--- app.js
+++ app.js
@@ -4,6 +4,27 @@ const logger = require('morgan');
 
 const userRoutes = require('./api/routes/users');
 
+// logger
 app.use(logger('dev'));
+
+// Routes to handle requests
 app.use('/users', userRoutes);
+
+
+app.use((req, res, next) => {
+    const err = new Error('Not found');
+    err.status= 404;
+    next(err);
+})
+
+app.use((err, req, res, next) => {
+
+    res.status(err.status || 500);
+    res.json({
+        error:{
+            message: err.message
+        }
+    })
+})
+
 module.exports = app;
