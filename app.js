6c4e22e4831f8862870946152f57a9e2b9fc5701
Gurvinder singh <gurayasaab@hotmail.com>
2019-03-22 12:51:05 -0700

    refractor(logger): adding logger

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
+const logger = require('morgan');
 
 const userRoutes = require('./api/routes/users');
 
+app.use(logger('dev'));
 app.use('/users', userRoutes);
 module.exports = app;
