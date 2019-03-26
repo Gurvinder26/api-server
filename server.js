384379fa661c79b46f842efc3728bc070924a88d
Gurvinder singh <gurayasaab@hotmail.com>
2019-03-25 23:12:45 -0700

    refractor(nodemon): add port env variable

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const http = require('http');
 const app = require('./app');
 
-const port = process.env.port || 8080;
+const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 
 server.listen(port);
+console.log('Server start listening on port: '+ port);
