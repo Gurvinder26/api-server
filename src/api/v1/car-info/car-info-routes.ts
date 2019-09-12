4bfc30ccc262d3aee368973473f31d85c02fa97a
Gurvinder singh <gurayasaab@hotmail.com>
2019-09-11 21:34:45 -0700

    feature(login): login functionality with password

--- src/api/v1/car-info/car-info-routes.ts
+++ src/api/v1/car-info/car-info-routes.ts
@@ -1,10 +1,10 @@
-import * as express from 'express';
-import * as carInfoController from './car-info-controller';
+import * as express from "express";
+import * as carInfoController from "./car-info-controller";
+const checkAuth = require("../../auth/check-auth");
 
 const router = express.Router();
-router.get('/', carInfoController.getAllCars);
-router.post('/', carInfoController.addNewCar);
-router.get('/:id', carInfoController.getCarByVin);
+router.get("/", checkAuth, carInfoController.getAllCars);
+router.post("/", checkAuth, carInfoController.addNewCar);
+router.get("/:id", checkAuth, carInfoController.getCarByVin);
 
 export const carInfoRoutes = router;
-
--- src/api/v1/user/user-model.ts
+++ src/api/v1/user/user-model.ts
@@ -12,6 +12,7 @@ const userSchema: mongoose.Schema = new mongoose.Schema({
         unique: true,
         match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
     },
+    password: {type: String, required: true},
     confirmed: {type: Boolean, required: true, default: false }
 });
 
